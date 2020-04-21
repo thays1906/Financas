14b12b0901f5bdbf0bebdb0fdb089d85820feb01
Thays Silva <thaysiinha19@hotmail.com>
2020-04-20 23:27:14 -0300

    Banco de Dados (tables+proc)

--- GerenciamentoFinanças/DAL/pReceita.vb
+++ GerenciamentoFinanças/DAL/pReceita.vb
@@ -1,3 +1,20 @@
-﻿Public Class pReceita
+﻿Imports GFT.Util
+Imports GFT.Util.BancoDados
 
+Public Class pReceita
+    Public Shared OPERACAO As Campo = New Campo("OPERACAO", DbType.String, 4)
+    Public Const PROCEDURE As String = "pReceita"
+
+    Class pConsulta
+        Public Shared cConsulta As Campo = New Campo("cConsulta", DbType.Decimal, 10, 0)
+        Public Shared dtCriacao As Campo = New Campo("dtCriacao", DbType.DateTime, 10)
+        Public Shared rReceita As Campo = New Campo("rReceita", DbType.String, 50)
+        Public Shared cValorReceita As Campo = New Campo("cValorReceita", DbType.Decimal, 18, 0)
+        Public Shared rDespesa As Campo = New Campo("rDespesa", DbType.String, 50)
+        Public Shared cValorDespesa As Campo = New Campo("cValorDespesa", DbType.Decimal, 18, 0)
+    End Class
+
+    Public Sub InserirReceita()
+        Dim bDados As BancoDados
+    End Sub
 End Class
--- GerenciamentoFinanças/Formulários/frmNovaDespesa.vb
+++ GerenciamentoFinanças/Formulários/frmNovaDespesa.vb
@@ -3,6 +3,7 @@ Public Class frmNovaDespesa
     Private Sub frmNovaDespesa_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
         Cor(Me, Collor.Preto)
+
     End Sub
 
     Private Sub btnFechar_Click(sender As Object, e As EventArgs) Handles btnFechar.Click
